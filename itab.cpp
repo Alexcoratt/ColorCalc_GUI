db127fccccf9eb62f37bb3beebc12087a39c0ab1
Alexander Smirnov <alexsm.w@yandex.ru>
2023-12-02 14:47:31 +0300

    Fixed the bug of non showed error messages

--- itab.cpp
+++ itab.cpp
@@ -7,30 +7,15 @@ void ITab::clear() {
 }
 
 void ITab::createPreset(QString const & name) {
-    try {
-        getDataManager()->createPreset(name.toStdString());
-    } catch (std::exception const & err) {
-        std::cerr << err.what() << std::endl;
-    }
-
+    getDataManager()->createPreset(name.toStdString());
     update();
 }
 void ITab::updatePreset(QString const & name) {
-    try {
-        getDataManager()->updatePreset(name.toStdString());
-    } catch (std::exception const & err) {
-        std::cerr << err.what() << std::endl;
-    }
-
+    getDataManager()->updatePreset(name.toStdString());
     update();
 };
 void ITab::removePreset(QString const & name) {
-    try {
-        getDataManager()->removePreset(name.toStdString());
-    } catch (std::exception const & err) {
-        std::cerr << err.what() << std::endl;
-    }
-
+    getDataManager()->removePreset(name.toStdString());
     update();
 };
 
